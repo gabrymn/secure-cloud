b540b054e79bae90fa73c3776606cc18c8a74106
gabrielemigliorinii <gabrieledevs@gmail.com>
2022-04-29 22:09:14 +0000

    Added Bootstrap

--- www/src/front-end/public/index.php
+++ www/src/front-end/public/index.php
@@ -48,27 +48,63 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 </head>
 <body>
-    <br>
-    <center><div style="border:2px solid black;border-radius:25px;width:60%">
-        <h1>HOME PAGE</h1>
-        <h2><a href="login">Login</a><h2>
-        <h2><a href="signup">Signup</a><h2>
-    </div></center>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://www.mywebs.altervista.org/Final/resources/api.js"></script>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="">HOME</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Crittografia</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="log.php">Accedi</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="reg.php">Registrati</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+    </nav>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
 
+<style>
+    
+    @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap');
+
+    * {
+        font-family: 'Quicksand', sans-serif;
+    }
+
+    body {
+        background-color: rgb(60,60,60);
+    }
+
+    ::selection {
+        background-color: rgb(150,150,150);
+    }
+
+</style>
+
 <script>    
 
-    $('document').ready(() => {
+    /*$('document').ready(() => {
         if (getCookie('ALLOW') === ''){
             let text = 'Accetti i cookie?';
             if (confirm(text) === true) 
                 setCookie('ALLOW', '1');
         }
-    });
+    });*/
 
 </script>
