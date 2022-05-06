4ed0123038454efbd56d65113488aba16777c9fa
gabrielemigliorinii <gabrieleusm@icloud.com>
2022-05-06 22:40:02 +0200

    Update CSS

--- www/src/front-end/public/pvt.php
+++ www/src/front-end/public/pvt.php
@@ -54,29 +54,60 @@
 
 ?>
 
+<!------ START BOOTSTRAP FORM ---------->
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ END BOOTSTRAP FORM ---------->
+
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
+    <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/shared.css">
-    <title>Private area</title>
-    <link href="../img/icon.svg" rel="icon" type="image/x-icon" sizes="32x32">
-</head>
-<body>
-    <h1>Private area of <?php echo $email; ?> </h1>
-    <h3><a href='../../back-end/class/out.php'>logout</a></h3>
-    <center><input type="file" id="ID_FILE_UPLOADER"></center><br>
-    <div id="C_FILES" class="FILE_CARDS"></div>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Private Area</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="../css/shared.css">
+        <link rel="stylesheet" href="../css/login.css">
+        <link href="../img/icon.svg" rel="icon" type="image/x-icon" >
+    </head>
+    <body>
 
-    <br>
-    <h3>2FA</h3><input id="OTP_YN" type="checkbox">
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/aes.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/sha256.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-</body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php"><?php echo $email; ?></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="../../back-end/class/out.php">Logout</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#" id="ID_UPLOAD">Upload file</a>
+                        </li>
+                        <li class="nav-item">
+                            <input id="OTP_YN" type="checkbox">
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <input type="file" id="ID_FILE_UPLOADER" style="display:none">
+        <br><br><div id="C_FILES" class="FILE_CARDS"></div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/aes.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/sha256.js"></script>
+        <script src="../js/login.js"></script>
+        <script src="../js/shared.js"></script>
+    </body>
 </html>
 
 <script type="module">
@@ -215,6 +246,10 @@
         })
     }
 
+    $("#ID_UPLOAD").on('click', () => {
+        $('#ID_FILE_UPLOADER').trigger('click');
+    });
+
 </script>
 
 <style>
@@ -233,7 +268,6 @@
     a, h1, h3 {
 
         color: white;
-        font-size: 1.5rem;
     }
 
     input {
--- www/src/front-end/public/verify.php
+++ www/src/front-end/public/verify.php
@@ -63,6 +63,13 @@
 
 ?>
 
+<!------ START BOOTSTRAP FORM ---------->
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ END BOOTSTRAP FORM ---------->
+
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
