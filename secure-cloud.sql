83b84f2203b109df2c51b3536d738db0c59d8ae3
gabrielemigliorinii <97559996+gabrielemigliorinii@users.noreply.github.com>
2022-05-06 21:15:44 +0200

    Update secure-cloud.sql

--- secure-cloud.sql
+++ secure-cloud.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Mag 06, 2022 alle 11:53
--- Versione del server: 10.1.38-MariaDB
--- Versione PHP: 7.3.2
+-- Creato il: Mag 06, 2022 alle 21:13
+-- Versione del server: 10.4.22-MariaDB
+-- Versione PHP: 8.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -32,7 +31,19 @@ CREATE TABLE `account_recovery` (
   `id_user` int(11) NOT NULL,
   `htkn` varchar(80) NOT NULL,
   `expires` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `account_verify`
+--
+
+CREATE TABLE `account_verify` (
+  `id_user` int(11) NOT NULL,
+  `htkn` varchar(64) NOT NULL,
+  `expires` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -44,7 +55,7 @@ CREATE TABLE `env` (
   `id` varchar(10) NOT NULL,
   `key` varchar(30) NOT NULL,
   `value` varchar(120) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -56,14 +67,27 @@ CREATE TABLE `remember` (
   `htkn` varchar(64) NOT NULL,
   `expires` datetime NOT NULL,
   `id_user` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dump dei dati per la tabella `remember`
 --
 
 INSERT INTO `remember` (`htkn`, `expires`, `id_user`) VALUES
-('c22df5c28ca0de641d244b9957e889dd5d697ec2ef069dc9843585ce7d5bde67', '2022-05-24 11:29:36', 1);
+('f7decb918d44a14b111afebde41d5d6256f04fedd90b47eebabb52b69eee0288', '2022-05-23 10:29:56', 6);
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `uploads`
+--
+
+CREATE TABLE `uploads` (
+  `id_file` varchar(64) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `size` int(11) NOT NULL,
+  `datet` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -78,14 +102,14 @@ CREATE TABLE `users` (
   `logged_with` varchar(20) NOT NULL,
   `2FA` int(11) NOT NULL,
   `verified` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dump dei dati per la tabella `users`
 --
 
 INSERT INTO `users` (`id`, `email`, `pass`, `logged_with`, `2FA`, `verified`) VALUES
-(10, 'gabrieledevs@gmail.com', '$2y$10$JNIyzdyzl5pVu288BzTspOq0WfE8hEw5aGu9qTTe.uhYnCnVZUlz.', 'EMAIL', 0, 0);
+(31, 'gabrieledevs@gmail.com', '$2y$10$arfSJGV6rikJKw5mCxlKPuuDc7xc5JbmzBWcK2m6aaqU3Nml5TxbG', 'EMAIL', 0, 1);
 
 --
 -- Indici per le tabelle scaricate
@@ -105,7 +129,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT per la tabella `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
