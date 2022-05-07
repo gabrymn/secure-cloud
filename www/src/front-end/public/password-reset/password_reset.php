49cf8a5b9e2ef78e0be12a947e44b9ebee77e503
gabrielemigliorinii <gabrieleusm@icloud.com>
2022-05-07 23:09:33 +0200

    Update <title>

--- www/src/front-end/public/password-reset/password_reset.php
+++ www/src/front-end/public/password-reset/password_reset.php
@@ -157,12 +157,12 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>SECURE CLOUD</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" href="../../css/shared.css">
         <link rel="stylesheet" href="../../css/login.css">
         <link href="../../img/icon.svg" rel="icon" type="image/x-icon" >
-        <title>Reset your password</title>
     </head>
 
     <body>
